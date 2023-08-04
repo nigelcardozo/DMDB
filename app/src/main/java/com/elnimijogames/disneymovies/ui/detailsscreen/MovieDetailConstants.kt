c938ea00cc2f555d69fcdbbef189452d8a51107b
nigel cardozo <games@elnimijo.com>
2023-08-04 11:31:49 +0100

    Made overview text expandable

--- app/src/main/java/com/elnimijogames/disneymovies/ui/detailsscreen/MovieDetailConstants.kt
+++ app/src/main/java/com/elnimijogames/disneymovies/ui/detailsscreen/MovieDetailConstants.kt
@@ -3,4 +3,6 @@ package com.elnimijogames.disneymovies.ui.detailsscreen
 object MovieDetailConstants {
     const val GOOD_MOVIE = 7
     const val AVERAGE_MOVIE = 5
+    const val MAX_LINES_OVERVIEW = 20
+    const val MIN_LINES_OVERVIEW = 3
 }
--- app/src/main/java/com/elnimijogames/disneymovies/ui/detailsscreen/MovieDetailsScreen.kt
+++ app/src/main/java/com/elnimijogames/disneymovies/ui/detailsscreen/MovieDetailsScreen.kt
@@ -1,5 +1,6 @@
 package com.elnimijogames.disneymovies.ui.detailsscreen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -17,6 +18,10 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,6 +36,8 @@ import com.elnimijogames.disneymovies.model.responses.MovieDetailsResponse
 import com.elnimijogames.disneymovies.model.responses.ProductionCompanies
 import com.elnimijogames.disneymovies.ui.detailsscreen.MovieDetailConstants.AVERAGE_MOVIE
 import com.elnimijogames.disneymovies.ui.detailsscreen.MovieDetailConstants.GOOD_MOVIE
+import com.elnimijogames.disneymovies.ui.detailsscreen.MovieDetailConstants.MAX_LINES_OVERVIEW
+import com.elnimijogames.disneymovies.ui.detailsscreen.MovieDetailConstants.MIN_LINES_OVERVIEW
 import com.elnimijogames.disneymovies.ui.theme.DisneyMoviesTheme
 import java.text.DecimalFormat
 import kotlin.math.roundToInt
@@ -39,6 +46,8 @@ private val BASE_URL = "https://image.tmdb.org/t/p/w370_and_h556_multi_faces/"
 
 @Composable
 fun MovieDetailsScreen(movieDetailsResponse: MovieDetailsResponse, textColor: Color = Color.White) {
+    var isOverviewExpanded by remember { mutableStateOf(false) }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -115,7 +124,8 @@ fun MovieDetailsScreen(movieDetailsResponse: MovieDetailsResponse, textColor: Co
                     Modifier
                         .padding(start = 4.dp, end = 4.dp, bottom = 2.dp)
                         .fillMaxWidth()
-                        .height(IntrinsicSize.Min),
+                        .height(IntrinsicSize.Min)
+                        .clickable { isOverviewExpanded = !isOverviewExpanded },
                     colors = CardDefaults.cardColors(
                         containerColor = Color.Black.copy(alpha = 0.6f))
                 ) {
@@ -124,7 +134,7 @@ fun MovieDetailsScreen(movieDetailsResponse: MovieDetailsResponse, textColor: Co
                             .padding(start = 10.dp, top = 10.dp, bottom = 10.dp),
                         text = movieDetailsResponse.overview.toString(),
                         overflow = TextOverflow.Ellipsis,
-                        maxLines = 4,
+                        maxLines = if (isOverviewExpanded) MAX_LINES_OVERVIEW else MIN_LINES_OVERVIEW,
                         style = MaterialTheme.typography.displaySmall,
                         color = textColor
                     )
