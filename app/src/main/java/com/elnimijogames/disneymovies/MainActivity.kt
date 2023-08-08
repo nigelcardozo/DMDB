b90b2b688a76a6a032123fe59fcb39084fd9c72b
nigel cardozo <games@elnimijo.com>
2023-08-08 09:27:58 +0100

    Completed transition work

--- app/src/main/java/com/elnimijogames/disneymovies/MainActivity.kt
+++ app/src/main/java/com/elnimijogames/disneymovies/MainActivity.kt
@@ -1,22 +1,26 @@
 package com.elnimijogames.disneymovies
 
- import android.os.Bundle
- import androidx.activity.ComponentActivity
- import androidx.activity.compose.setContent
- import androidx.compose.runtime.Composable
- import androidx.hilt.navigation.compose.hiltViewModel
- import androidx.navigation.NavType
- import androidx.navigation.compose.NavHost
- import androidx.navigation.compose.composable
- import androidx.navigation.compose.rememberNavController
- import androidx.navigation.navArgument
- import com.elnimijogames.disneymovies.model.StringResourceProviderImpl
- import com.elnimijogames.disneymovies.ui.detailsscreen.MovieDetailsScreen
- import com.elnimijogames.disneymovies.ui.detailsscreen.MovieDetailsViewModel
- import com.elnimijogames.disneymovies.ui.movielist.MovieListScreen
- import com.elnimijogames.disneymovies.ui.splashscreen.SplashScreen
- import com.elnimijogames.disneymovies.ui.theme.DisneyMoviesTheme
- import dagger.hilt.android.AndroidEntryPoint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.elnimijogames.disneymovies.model.StringResourceProviderImpl
+import com.elnimijogames.disneymovies.ui.detailsscreen.MovieDetailsScreen
+import com.elnimijogames.disneymovies.ui.detailsscreen.MovieDetailsViewModel
+import com.elnimijogames.disneymovies.ui.movielist.MovieListScreen
+import com.elnimijogames.disneymovies.ui.splashscreen.SplashScreen
+import com.elnimijogames.disneymovies.ui.theme.DisneyMoviesTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+private var navigatedToMovieDetailsScreen = false
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -36,14 +40,59 @@ class MainActivity : ComponentActivity() {
 @Composable
 private fun DMDBApp(stringResourceProvider: StringResourceProviderImpl) {
     val navController = rememberNavController()
+    navController.enableOnBackPressed(true)
 
     NavHost(navController, startDestination = "splash_screen") {
-        composable(route = "splash_screen") {
+        composable(
+            route = "splash_screen",
+            enterTransition = {
+                navigatedToMovieDetailsScreen = false
+                slideIntoContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Right,
+                    animationSpec = tween(500)
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Left,
+                    animationSpec = tween(500)
+                )
+            }) {
             SplashScreen {
                 navController.navigate("movie_list_screen")
             }
         }
-        composable(route = "movie_list_screen") {
+        composable(
+            route = "movie_list_screen",
+            enterTransition = {
+                //if (("splash_screen").equals(navController.previousBackStackEntry?.destination?.route)) {
+                if (!navigatedToMovieDetailsScreen) {
+                    slideIntoContainer(
+                        towards = AnimatedContentTransitionScope.SlideDirection.Companion.Left,
+                        animationSpec = tween(500)
+                    )
+                }
+                else {
+                    slideIntoContainer(
+                        towards = AnimatedContentTransitionScope.SlideDirection.Companion.Right,
+                        animationSpec = tween(500)
+                    )
+                }
+            },
+            exitTransition = {
+                if (("movie_list_screen") == navController.previousBackStackEntry?.destination?.route) {
+                    slideOutOfContainer(
+                        towards = AnimatedContentTransitionScope.SlideDirection.Companion.Left,
+                        animationSpec = tween(500)
+                    )
+                } else {
+                    slideOutOfContainer(
+                        towards = AnimatedContentTransitionScope.SlideDirection.Companion.Right,
+                        animationSpec = tween(500)
+                    )
+                }
+
+            }) {
             MovieListScreen() { id ->
                 navController.navigate("destination_movie_details_screen/${id}")
             }
@@ -52,10 +101,25 @@ private fun DMDBApp(stringResourceProvider: StringResourceProviderImpl) {
             route = "destination_movie_details_screen/{id}",
             arguments = listOf (
                 navArgument("id") { type = NavType.IntType }
-            )
+            ),
+            enterTransition = {
+                navigatedToMovieDetailsScreen = true
+                slideIntoContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Left,
+                    animationSpec = tween(500)
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Right,
+                    animationSpec = tween(500)
+                )
+            }
         ) {
             val viewModel: MovieDetailsViewModel = hiltViewModel()
-            MovieDetailsScreen(viewModel.movieDetailsState.value)
+            MovieDetailsScreen(
+                viewModel.movieDetailsState.value
+            )
         }
     }
 }
